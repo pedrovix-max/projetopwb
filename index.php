9c5205655a8640384e7fe6aaa8a5a906491cc50d
Laboratorio <laboratorio@escola.local>
2026-08-12 10:49:07 -0300

    Formulari de login

--- index.php
+++ index.php
@@ -1,69 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<?php include "cabecalho.php"; ?>
+<style>
+    div{
+        border:1px solid #008800;
+    }
+    </style>
+<div class="row">
+    <div class="col-md-4"></div>
 
-    <title>Document</title>
-</head>
-<body>
-    
-    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg5Gcp78sr5KGRn44zVgZrCmNucZ0g0jf5V6NgzZTyvQ&s=10" alt="Logo" width="50" height="45" class="d-inline-block align-text-top">
-SLK</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Cadastros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Relatórios</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Selecione a opção
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-                <a class="dropdown-item" href="#">Usuários</a></li>
-            <li>
-                <a class="dropdown-item" href="#"> Produtos </a></li>
-            <li>
-                <hr class="dropdown-divider"></li>
-            <li>
-                <a class="dropdown-item" href="#"> Clientes </a></li>
-          </ul>
-        </li>
-    
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+   
+    <div class="col-md-4"><!--- coluna do meio--->
+        <div class="card">
+            <div class="card-body">
+                <form action="" method="post">
+                
+                <label for="login">Username</label>
+                    <input class="form-control"
+                    type="text"
+                    name="login"
+                    id="login"/>
+                 <label for="senha">Senha</label>
+                    <input class="form-control"
+                    type="password"
+                    name="senha"
+                    id="senha"/>
+                    <div class="row">
+                        <div class="col-md-6"></div>
+                    
+                    <div class="col-md-6"></div>
+
+</form> 
+             </div>
+        </div>      
     </div>
-  </div>
-</nav>
-    <h1>SYSTEM<h1>
-            <div class="alert alert-success" role="alert">
-  A simple success alert—check it out!
-</div>
-  <div class="card">
-  <div class="card-header bg-primary">
-   <span class="text-white"> <strong>Pesquisar</strong> </span>
-  </div>
-  <div class="card-body">
+   
 
-    <p class="card-text"> Aqui será um menu de pesquisa</p>
-    <a href="#" class="btn btn-primary"> Pesquisar </a>
-  </div>
+    <div class="col-md-4"></div>
 </div>
 
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
- 
-</body>
-</html>
+
+<?php include "rodape.php"; ?>
---
+++ rodape.php
@@ -0,0 +1,4 @@
+</div> <!----- fechamento do container ---->
+<script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
